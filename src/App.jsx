fc8cc25d133a82b69002dfe61c084c7620014efb
Fabián Romero <fabo.romero97@gmail.com>
2021-07-05 17:42:07 -0300

    add libs and components for the ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
+import SearchBar from './components/SearchBar'
+import CommercesList from './components/Table/CommercesList'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>search bar</h1>
+      <SearchBar/>
+      <CommercesList/>
     </div>
   )
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Box, Input} from '@chakra-ui/react'
+export default function SearchBar() {
+    return (
+        <Box>
+             <Input placeholder="large size" size="lg" />
+        </Box>
+    )
+}
